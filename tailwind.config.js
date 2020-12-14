1c319ca1a5a2c14d59bf6dd429fe15ff7a619080
rogermedico <roger.medico@gmail.com>
2020-12-14 20:17:13 +0100

    tailwind applied to navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Raleway', 'arial', 'helvetica', 'sans-serif'],
+      'serif': ['Roboto\\ Slab', 'georgia', 'times', 'serif'],
+      'special': ['Gloria\\ Hallelujah', 'cursive']
+    },
+    extend: {
+      colors: {
+        'primary': '#ffc600',
+        'secondary': '#8c00ff',
+      },
+      // backgroundImage: {
+      //   'stage': "url('./src/assets/images/background.jpg')",
+      // }
+    },
   },
   variants: {
     extend: {},
