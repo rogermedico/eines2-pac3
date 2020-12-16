5a7b06f01ccc1b5c568fddc4e54202af9955c34d
rogermedico <roger.medico@gmail.com>
2020-12-16 12:39:43 +0100

    tailwind artists

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,15 +10,26 @@ module.exports = {
       'special': ['Gloria\\ Hallelujah', 'cursive']
     },
     boxShadow: {
-      'tickets': '0 4px 8px 0 rgba(256, 198, 0, .1), 0 6px 20px 0 rgba(256, 198, 0, .1)'
+      'tickets': '0 4px 8px 0 rgba(256, 198, 0, .1), 0 6px 20px 0 rgba(256, 198, 0, .1)',
+      'artists': '0 4px 8px 0 rgba(256, 198, 0, .7), 0 6px 20px 0 rgba(256, 198, 0, .7)'
     },
+    container: {
+      center: true,
+    },
+    flex: {
+      'artists-xs': '0 1 80%',
+      'artists-md': '0 1 40%',
+      'artists-lg': '0 1 25%'
+    },
+
     extend: {
       colors: {
         'primary': '#ffc600',
         'secondary': '#8c00ff',
+        'background-color-light': '#101010',
       },
       // backgroundImage: {
-      //   'stage': "url('./src/assets/images/background.jpg')",
+      //   'stage': "url('/src/assets/images/background.jpg')",
       // },
       gridTemplateColumns: {
         'home': 'minmax(50px, auto) repeat(6, 1fr) minmax(50px, auto)'
@@ -29,7 +40,9 @@ module.exports = {
       spacing: {
         'video': '56.25%'
       },
-
+      maxWidth: {
+        '250px': '250px',
+      },
     },
   },
   variants: {
